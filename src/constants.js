86c09692fc9ea13a99e9fb6e147abaeb27bcec51
mingder wang <mingderwang@me.com>
2026-02-02 16:01:03 +0800

    use local relay

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const PUBSUB_PEER_DISCOVERY = "browser-peer-discovery";
+export const PUBSUB_AUDIO = "audio-treehole";
+export const RELAY_MULTIADDR =
+  "/ip4/127.0.0.1/tcp/9001/ws/p2p/12D3KooWSmXgTS8o7F8xpkf6WsrLFPtcWYguLiUAVyv2h7CvoGRV";
+// "/dns6/14490944-bced-4f7a-90d0-5469826d6d01.pub.instances.scw.cloud/tcp/443/wss/p2p/12D3KooW9scFmH8UkU39qG5WKWY5WW3MRTERUqLPCoqLQ1oAPpS4";
--- src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const PUBSUB_PEER_DISCOVERY = "browser-peer-discovery";
-export const PUBSUB_AUDIO = "audio-treehole";
-export const RELAY_MULTIADDR =
-  "/dns6/14490944-bced-4f7a-90d0-5469826d6d01.pub.instances.scw.cloud/tcp/443/wss/p2p/12D3KooW9scFmH8UkU39qG5WKWY5WW3MRTERUqLPCoqLQ1oAPpS4";
